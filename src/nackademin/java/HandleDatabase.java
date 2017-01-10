8b7e1962e98955bb0ed29a506fecf3f9a8796333
Patricio <patricio89morales5@hotmail.com>
2017-01-10 20:34:52 +0100

    WAII U NO WORK?!

--- src/nackademin/java/HandleDatabase.java
+++ src/nackademin/java/HandleDatabase.java
@@ -9,7 +9,7 @@ public class HandleDatabase {
 
     public String getData() {
         try {
-            File dbContacts = new File("C:\\Users\\patri\\Desktop\\ServerCatalogue\\src\\contacts.csv");
+            dbContacts = new File("C:\\Users\\patri\\Desktop\\ServerCatalogue\\src\\contacts.csv");
             FileReader fileReader = new FileReader(dbContacts);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             while (bufferedReader.readLine() != null) {
--- src/nackademin/java/Server.java
+++ src/nackademin/java/Server.java
@@ -20,36 +20,30 @@ public class Server {
                 Socket clientSocket = serverSocket.accept();
 
                 new Thread(
-                        new Runnable() {
+                        () -> {
+                            try {
+                                InputStream inputStream = clientSocket.getInputStream();
+                                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
-                            public void run() {
-                                try {
-                                    InputStream inputStream = clientSocket.getInputStream();
-                                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                                OutputStream outputStream = clientSocket.getOutputStream();
+                                PrintWriter writer = new PrintWriter(outputStream);
 
-                                    OutputStream outputStream = clientSocket.getOutputStream();
-                                    PrintWriter writer = new PrintWriter(outputStream);
-
-                                    BufferedReader reader = new BufferedReader(inputStreamReader);
-                                    for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                                        System.out.println(line);
-                                        writer.println("Message received: " + line);
-                                        writer.flush();
-                                        if (line.equals("getall")) {
-                                            String tempString = handleDatabase.getData();
-                                            writer.println(tempString);
-                                        }else if (line.equals("exit")){
-                                            break;
-                                        }
+                                BufferedReader reader = new BufferedReader(inputStreamReader);
+                                for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                                    System.out.println("Client wrote " + line);
+                                    if (line.equals("getall")) {
+                                        String tempString = handleDatabase.getData();
+                                        writer.println(tempString);
+                                    }else if (line.equals("exit")){
+                                        break;
                                     }
-                                    reader.close();
-                                    writer.close();
-                                    clientSocket.close();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
                                 }
+                                reader.close();
+                                writer.close();
+                                clientSocket.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
-
                         }
                 ).start();
 
